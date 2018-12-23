8bce09a3ee6b0524ab7e5c3e5899a5fcc2686a4e
naaltunian <naaltunian@icloud.com>
2018-12-23 15:38:07 -0500

    connect database

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const database = require("./config.js");
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+// database connection
+const url = `mongodb://${database.dbUser}:${database.dbPassword}@ds243054.mlab.com:43054/web-connect`;
+mongoose.connect(url, { useNewUrlParser: true })
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("running"));
 
