18c2fa459a5257ef770037458977831ae428d4ee
naaltunian <naaltunian@icloud.com>
2018-12-24 11:49:03 -0500

    set up bcrypt and user login/password check

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const key = require('../../config.js');
 
 // user model
 const User = require('../../models/User');
@@ -34,6 +36,33 @@ router.post('/register', (req, res) => {
                 })
             });
         }
+    });
+});
+
+// user login
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email})
+    .then(user => {
+        if(!user){
+            return res.status(404).json({email: 'User not found'});
+        }
+        // password check
+        bcrypt.compare(password, user.password)
+        .then(userMatch => {
+            if(userMatch){
+                const payload = { id: user.id, name: user.name, avatar: user.avatar }
+                jwt.sign(payload, key.secretKey, { expiresIn: 3600 }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    })
+                });
+            } else {
+                return res.status(400).json({password: 'Incorrect Password'})
+            }
+        })
     });
 });
 
